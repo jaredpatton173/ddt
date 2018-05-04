c9a6cf8cef3cd497017288dc9591a0aaa153da4a
Jared Patton <jaredpatton173@gmail.com>
2018-05-04 11:18:28 -0400

    First commit of liquibase database initialization.

---
+++ DataImporter/ddt.data.importer/src/main/java/ddt/data/importer/App.java
@@ -0,0 +1,13 @@
+package ddt.data.importer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
